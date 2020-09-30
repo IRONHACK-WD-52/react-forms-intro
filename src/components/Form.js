14ceacb5151296fa10a52f4f4a78eb16da40964c
rschpdr <pedro.resch@gmail.com>
2020-09-30 11:21:58 -0300

    done with hooks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,78 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "bulma/css/bulma.css";
 
-class Form extends React.Component {
-  state = {
+function Form() {
+  const [state, setState] = useState({
     name: "",
     email: "",
     password: "",
+  });
+
+  const handleChange = (event) => {
+    // CUIDADO! A funçāo de atualizaçāo de state do useState é destrutiva! Ela substitui o state atual pelo que estamos mandando, então, para nāo perder propriedades na atualizaçāo, precisamos fazer o spread das propriedades atuais
+    setState({
+      ...state,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
   };
 
-  handleChange = (event) => {
-    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
+    console.log(state);
   };
 
-  render() {
-    return (
-      <div className="container mt-5">
-        <form onSubmit={this.handleSubmit}>
-          {/* Name field */}
-          <div className="field">
-            <label className="label">Name</label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                name="name"
-                onChange={this.handleChange}
-                value={this.state.name}
-              />
-            </div>
+  return (
+    <div className="container mt-5">
+      <form onSubmit={handleSubmit}>
+        {/* Name field */}
+        <div className="field">
+          <label className="label">Name</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name="name"
+              onChange={handleChange}
+              value={state.name}
+            />
           </div>
-          {/* Email field */}
-          <div className="field">
-            <label className="label">E-mail</label>
-            <div className="control">
-              <input
-                className="input"
-                type="email"
-                name="email"
-                onChange={this.handleChange}
-                value={this.state.email}
-              />
-            </div>
+        </div>
+        {/* Email field */}
+        <div className="field">
+          <label className="label">E-mail</label>
+          <div className="control">
+            <input
+              className="input"
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={state.email}
+            />
           </div>
-          {/* Password field */}
-          <div className="field">
-            <label className="label">Password</label>
-            <div className="control">
-              <input
-                className="input"
-                type="password"
-                name="password"
-                onChange={this.handleChange}
-                value={this.state.password}
-              />
-            </div>
+        </div>
+        {/* Password field */}
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input
+              className="input"
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={state.password}
+            />
           </div>
+        </div>
 
-          {/* Submit button */}
-          <div className="field is-grouped">
-            <div className="control">
-              <button className="button is-link" type="submit">
-                Submit
-              </button>
-            </div>
+        {/* Submit button */}
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link" type="submit">
+              Submit
+            </button>
           </div>
-        </form>
-      </div>
-    );
-  }
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
